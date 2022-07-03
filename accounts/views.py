5cde084a1596c1b035303c4fe22de4483f98a811
Levi Shevick <levidshevick@gmail.com>
2022-07-03 18:40:18 -0400

    adds api app and accounts api view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+
+User = get_user_model()
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 app_name = 'api_v1'
 
 urlpatterns = [
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
