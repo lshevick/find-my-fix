6b68a346060114126821b47a2f3c6b61c0271028
Levi Shevick <levidshevick@gmail.com>
2022-07-07 14:08:05 -0400

    implementing crud

--- frontend/static/src/components/Dashboard.js
+++ frontend/static/src/components/Dashboard.js
@@ -39,25 +39,35 @@ const Dashboard = () => {
       <div className="fixed inset-0" aria-hidden="true" />
       <div className="fixed inset-0 flex items-center justify-center p-4">
         <Dialog.Panel className="mx-auto max-w-sm rounded bg-white p-10">
-            <div className="w-48 h-48 overflow-hidden">
-                <img src={car.image} alt="car" className="object-cover"/>
-            </div>
+          <div className="w-48 h-48 overflow-hidden">
+            <img src={car.image} alt="car" className="object-cover" />
+          </div>
           <div className="flex border-b-2 border-stone-600">
-          <h2 className="mx-0.5">{car.year}</h2>
-          <h2 className="mx-0.5">{car.make}</h2>
-          <h2 className="mx-0.5">{car.model}</h2>
+            <h2 className="mx-0.5">{car.year}</h2>
+            <h2 className="mx-0.5">{car.make}</h2>
+            <h2 className="mx-0.5">{car.model}</h2>
           </div>
           <div className="m-3">
             <h2 className="font-semibold underline">Services</h2>
             <ul>
-            {car.service_list && car.service_list.map(i => <li key={i} className='capitalize font-light text-sm list-disc'>{i}</li>)}
+              {car.service_list &&
+                car.service_list.map((i) => (
+                  <li
+                    key={i}
+                    className="capitalize font-light text-sm list-disc"
+                  >
+                    {i}
+                  </li>
+                ))}
             </ul>
           </div>
           <div className="w-full flex justify-between">
-          <button type="button" onClick={() => setIsOpen(false)}>
-            Close
-          </button>
-          <button type="button" onClick={() => setIsEditing(!isEditing)}>{isEditing ? 'Cancel' : 'Edit'}</button>
+            <button type="button" onClick={() => setIsOpen(false)}>
+              Close
+            </button>
+            <button type="button" onClick={() => setIsEditing(!isEditing)}>
+              {isEditing ? "Cancel" : "Edit"}
+            </button>
           </div>
         </Dialog.Panel>
       </div>
@@ -70,24 +80,39 @@ const Dashboard = () => {
       <div className="fixed inset-0 flex items-center justify-center p-4">
         <Dialog.Panel className="mx-auto max-w-sm rounded bg-white p-10">
             <div className="w-48 h-48 overflow-hidden">
-                <img src={car.image} alt="car" className="object-cover"/>
+              <img src={car.image} alt="car" className="object-cover" />
             </div>
-          <div className="flex border-b-2 border-stone-600">
-          <h2 className="mx-0.5">{car.year}</h2>
-          <h2 className="mx-0.5">{car.make}</h2>
-          <h2 className="mx-0.5">{car.model}</h2>
-          </div>
-          <div className="m-3">
-            <h2 className="font-semibold underline">Services</h2>
-            <ul>
-            {car.service_list && car.service_list.map(i => <li key={i} className='capitalize font-light text-sm list-disc'>{i}</li>)}
-            </ul>
-          </div>
+            <div className="flex border-b-2 border-stone-600">
+              <h2 className="mx-0.5">{car.year}</h2>
+              <h2 className="mx-0.5">{car.make}</h2>
+              <h2 className="mx-0.5">{car.model}</h2>
+            </div>
+            <div className="m-3">
+              <h2 className="font-semibold underline">Services</h2>
+              <ul>
+                {car.service_list &&
+                  car.service_list.map((i) => (
+                    <li
+                      key={i}
+                      className="capitalize font-light text-sm list-disc"
+                    >
+                      <div className="w-full flex justify-between">
+                        {i}
+                        <button type="button" className="px-1 flex items-center h-5 text-white font-bold text-lg rounded hover:bg-red-600 bg-red-700">
+                          -
+                        </button>
+                      </div>
+                    </li>
+                  ))}
+              </ul>
+            </div>
           <div className="w-full flex justify-between">
-          <button type="button" onClick={() => setIsOpen(false)}>
-            Close
-          </button>
-          <button type="button" onClick={() => setIsEditing(!isEditing)}>{isEditing ? 'Cancel' : 'Edit'}</button>
+            <button type="button" onClick={() => setIsOpen(false)}>
+              Close
+            </button>
+            <button type="button" onClick={() => setIsEditing(!isEditing)}>
+              {isEditing ? "Cancel" : "Edit"}
+            </button>
           </div>
         </Dialog.Panel>
       </div>
@@ -112,8 +137,12 @@ const Dashboard = () => {
 
   return (
     <>
-      {isEditing? editCarModal : carModal}
-      <div className={`flex flex-col bg-stone-300 items-center justify-center w-full min-h-screen ${isOpen && `blur`}`}>
+      {isEditing ? editCarModal : carModal}
+      <div
+        className={`flex flex-col bg-stone-300 items-center justify-center w-full min-h-screen ${
+          isOpen && `blur`
+        }`}
+      >
         <div className="bg-stone-200 flex flex-col mx-auto p-3 my-5">
           <h2 className="border-b-2 border-stone-500">My Garage</h2>
           <ul>{garageDisplay}</ul>
