a84426ca219e772cb8740b46f6afbf2520937f02
Levi Shevick <levidshevick@gmail.com>
2022-07-07 16:09:50 -0400

    working on adding reviews

--- cars/models.py
+++ cars/models.py
@@ -6,7 +6,7 @@ class Car(models.Model):
     year = models.CharField(max_length=255)
     make = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
-    image = models.ImageField(blank=True)
+    image = models.ImageField(null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True)
     service_list = models.JSONField(blank=True)
 
--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from shops.models import Shop
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     body = models.TextField(max_length=255)
     service = models.JSONField()
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.body[:20]
